26c36ab9b264b98bb49a8b98902a2f04e1e5d79d
Quynh Nguyen <nhuquynh521@gmail.com>
2023-11-08 10:23:48 -0800

    first commit

---
+++ Python/json.py
@@ -0,0 +1,15 @@
+#
+import json
+
+
+#create the data dictionary
+
+data = {
+
+    'name': 'Quynh Nguyen'
+    'age': 24,
+    'city': 'Shoreline,WA',
+    'interests': ['hiking','cooking','videogames'],
+    'is_student': True
+
+}
